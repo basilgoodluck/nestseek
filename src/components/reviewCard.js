4d63c6c22182effdd944919af706c49f6d65eb54
Basil Goodluck <basilgoodluck22@gmail.com>
2024-04-22 18:19:11 +0100

    Updated

--- src/components/reviewCard.js
+++ src/components/reviewCard.js
@@ -5,7 +5,7 @@ import { FaStar } from "react-icons/fa";
 function ReviewCard() {
   return (
     <div>
-        <div className='flex items-center justify-center m-auto p-6' style={{minWidth: "650px", maxWidth: "650px",}}>
+        <div className='flex items-center justify-center m-auto p-6 w-24'>
             <div className='relative'>
                 <img src='./images/reviewImg-c.png' alt='reviewImg-c'/>
                 <div className='absolute -bottom-24 left-24 right-24 shadow-2xl p-6 bg-white rounded-3xl'>
