ad3ef11d5fb6836ebc79d4ed791b40507206fc1d
Basil Goodluck <basilgoodluck22@gmail.com>
2024-04-22 18:35:42 +0100

    Updated

--- src/components/review.js
+++ src/components/review.js
@@ -3,11 +3,11 @@ import ReviewCard from './reviewCard'
 
 function Rewiew() {
   return (
-    <div className='flex justify-center items-start mt-16'>
+    <div className='flex justify-center items-start mt-16 text-md'>
         <div className=''>
             <p className='text-orange-400 text-center'>---</p>
             <p className='text-orange-400 text-center'>See Our Review</p>
-            <h1 className='font-bold text-2xl text-center'>What Our Users Say About Us</h1>
+            <h1 className='font-bold lg:text-2xl text-center'>What Our Users Say About Us</h1>
             <div className='flex justify-between items-start flex-col lg:flex-row'>
                 <ReviewCard />
                 <ReviewCard />
